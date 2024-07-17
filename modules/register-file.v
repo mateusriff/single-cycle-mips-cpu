5f6544677a46fe33302b5e9344836ba6efb98314
Thiago Felipe de Franca Silva <tffs@windows.cin.ufpe.br>
2024-07-17 20:00:38 -0300

    Refactor Register File

--- modules/register-file.v
+++ modules/register-file.v
@@ -1,19 +1,20 @@
 module register_file (
     input clk,
-    input we3,
-    input [4:0] ra1, ra2, wa3,
-    input [31:0] wd3,
-    output [31:0] rd1, rd2
+    input write_enable,
+    input [4:0] read_address1, read_address2, write_address,
+    input [31:0] write_data,
+    output reg [31:0] read_data1, read_data2
 );
     
-    reg [31:0] rf[31:0];
+    reg [31:0] rf [31:0]; // Registrador de arquivos com 32 registradores de 32 bits
     
-    always @ (posedge clk) begin
-        if (we3) 
-            rf[wa3] <= wd3;
+    always @(posedge clk) begin
+        if (write_enable) begin
+            rf[write_addr] <= write_data;
+        end
     end
     
-    assign rd1 = (ra1 != 0) ? rf[ra1] : 0;
-    assign rd2 = (ra2 != 0) ? rf[ra2] : 0;
+    assign read_data1 = (read_addr1 != 0) ? rf[read_addr1] : 0;
+    assign read_data2 = (read_addr2 != 0) ? rf[read_addr2] : 0;
     
 endmodule
---
+++ modules/sign-extend.v
@@ -0,0 +1,4 @@
+module sign_extend (input [15:0] a,
+	output [31:0] y);
+	assign y = {{16{a[15]}}, a};
+endmodule
