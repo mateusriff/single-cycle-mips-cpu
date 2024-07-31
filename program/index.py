8393e6ef41efec7dcae1ec6bbe20e90361a8768e
Thiago Felipe <thiagofdefranca@gmail.com>
2024-07-31 19:52:30 -0300

    Update index.py

--- program/index.py
+++ program/index.py
@@ -1,5 +1,5 @@
 temperatura = 40  # Temperatura teste
-status_paciente = 1 # Se Pressupõem que o paciente ja esta com febre
+status_paciente = 0 # Se Pressupõem que o paciente ja esta com febre
 
-if temperatura < 38:  # Paciente com temperatura normal
-    status_paciente = 0
+if temperatura < 38:  
+    status_paciente = 1 # Paciente com temperatura normal
